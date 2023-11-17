59deceb7d654d06046f6652eeaca86d38cef397a
Marynryn <murmar95@gmail.com>
2023-11-17 19:25:14 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,7 +46,16 @@ export class App extends React.Component {
         contact.name && contact.name.toLowerCase().includes(contactsToLower)
     );
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts })
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const visualContacts = this.filterContacts();
     return (
